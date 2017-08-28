eb8d820fb3e17757589d172e874a1e49eb0274a8
GhostatSpirit <lykavin@hotmail.com>
2017-08-28 12:12:14 +0800

    improved geoquiz

--- Geoquiz/app/src/main/java/com/thegrasshoppers/geoquiz/QuizActivity.java
+++ Geoquiz/app/src/main/java/com/thegrasshoppers/geoquiz/QuizActivity.java
@@ -2,16 +2,25 @@ package com.thegrasshoppers.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+    private static final String KEY_ANSWERED_RECORDS = "AnsweredRecords";
 
     private Button mTrueButton;
     private Button mFalseButton;
+
+    private Button mPrevButton;
     private Button mNextButton;
     private TextView mQuestionTextView;
 
@@ -24,48 +33,80 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_asia, true),
     };
 
+    private boolean[] mAnsweredRecords;
+
     private int mCurrentIndex = 0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        updateQuestion();
-
 
         mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
+
+        // init answered records
+        if(savedInstanceState != null){
+            mAnsweredRecords = savedInstanceState.getBooleanArray(KEY_ANSWERED_RECORDS);
+        }
+        if(mAnsweredRecords == null){
+            mAnsweredRecords = new boolean[mQuestionBank.length];
+            Arrays.fill(mAnsweredRecords, false);
+        }
+
+
+        // check for existing current index value, if exists, retrieve it
+        if(savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+        updateQuestion();
+        mQuestionTextView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+
+
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast toast = Toast.makeText(
-                        QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_LONG
-                );
-                toast.setGravity(Gravity.TOP, 0, 0);
-                toast.show();
+                checkAnswer(true);
+                mAnsweredRecords[mCurrentIndex] = true;
+                updateChoiceButtons();
             }
         });
 
-
-        mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast toast = Toast.makeText(
-                        QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_LONG
-                );
-                toast.setGravity(Gravity.TOP, 0, 0);
-                toast.show();
+                checkAnswer(false);
+                mAnsweredRecords[mCurrentIndex] = true;
+                updateChoiceButtons();
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+
+        mPrevButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                if(mCurrentIndex < 0){
+                    mCurrentIndex += mQuestionBank.length;
+                }
+                updateQuestion();
+            }
+        });
+
         mNextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -75,8 +116,74 @@ public class QuizActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putBooleanArray(KEY_ANSWERED_RECORDS, mAnsweredRecords);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "calling onDestroy()");
+        super.onDestroy();
+    }
+
+
+
     private void updateQuestion(){
         int question = mQuestionBank[mCurrentIndex].getTestResId();
         mQuestionTextView.setText(question);
+        updateChoiceButtons();
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if(userPressedTrue == answerIsTrue){
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    private void updateChoiceButtons(){
+        boolean answered = mAnsweredRecords[mCurrentIndex];
+        if(answered){
+            mFalseButton.setEnabled(false);
+            mTrueButton.setEnabled(false);
+        } else {
+            mFalseButton.setEnabled(true);
+            mTrueButton.setEnabled(true);
+        }
     }
 }
